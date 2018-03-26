5ea26ab0c062b1ad6c0b504d118b45ff839f1386
Aleksei Ivankov <ivankoff@aport.ru>
2018-03-27 00:24:05 +1000

    completed task 4.2 [file: ch4/task_4_2.py]

---
+++ ch4/task_4_2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 4.2
+
+Преобразовать строку MAC из формата XXXX:XXXX:XXXX в формат XXXX.XXXX.XXXX
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+
+MAC = 'AAAA:BBBB:CCCC'
+print( 'Before\n\t' + MAC )
+
+MAC = MAC.replace(':', '.')
+print( 'Afer\n\t' + MAC )
