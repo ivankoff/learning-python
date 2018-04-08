e89ed19f3008e0ab7f8f5ef93db78c22a75f64c9
Aleksei Ivankov <ivankoff@aport.ru>
2018-04-09 00:39:53 +1000

    modified task 5.1 (added verification of network prefix length) [file: ch/task_5_1.py]

--- ch5/task_5_1.py
+++ ch5/task_5_1.py
@@ -23,7 +23,10 @@ Mask:
 network_address = ( input( 'Input IPv4 network address: ' ) ).strip()
 ip_address, prefix_length = network_address.split( '/' )
 
-print_template = '''
+if int( prefix_length ) > 32 :
+    print( ' Invalid network prefix! Must less or equal 32. ' )
+else :
+    print_template = '''
 Network:
  {0:<8} {1:<8} {2:<8} {3:<8}
  {0:0>8b} {1:0>8b} {2:0>8b} {3:0>8b}
@@ -34,17 +37,17 @@ Mask:
  {5:0>8b} {6:0>8b} {7:0>8b} {8:0>8b}
 '''
 
-mask = ( '1' * int(prefix_length) + '0' * ( 32 - int(prefix_length) ) )
-ip_address_octet = ip_address.split( '.' )
+    mask = ( '1' * int(prefix_length) + '0' * ( 32 - int(prefix_length) ) )
+    ip_address_octet = ip_address.split( '.' )
 
-print( '\n' * 2 + '-' * 60 )
-print( print_template.format( int( ip_address_octet[0] ),
-                              int( ip_address_octet[1] ),
-                              int( ip_address_octet[2] ),
-                              int( ip_address_octet[3] ),
-                              prefix_length,
-                              int( mask[0:8],   2),
-                              int( mask[8:16],  2),
-                              int( mask[16:24], 2),
-                              int( mask[24:],   2) ) )
-print( '-' * 60 )
+    print( '\n' * 2 + '-' * 60 )
+    print( print_template.format( int( ip_address_octet[0] ),
+                                  int( ip_address_octet[1] ),
+                                  int( ip_address_octet[2] ),
+                                  int( ip_address_octet[3] ),
+                                  prefix_length,
+                                  int( mask[0:8],   2),
+                                  int( mask[8:16],  2),
+                                  int( mask[16:24], 2),
+                                  int( mask[24:],   2) ) )
+    print( '-' * 60 )
